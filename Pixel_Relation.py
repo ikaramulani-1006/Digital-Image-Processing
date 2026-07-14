850251c821a8b543d8b990a092a1e549ee547e70
ikaramulani-1006 <ikaramulani2005@gmail.com>
2026-07-14 12:34:12 +0530

    Refactor pixel neighbor calculations and print statements

--- Pixel_Relation.py
+++ Pixel_Relation.py
@@ -1,30 +1,45 @@
-import numpy as np 
-image = np.array([[10, 20, 30, 40, 50], 
-                 [15, 25, 35, 45, 55],
-                 [20, 30, 40, 50, 60],
-                 [25, 35, 45, 55, 65],
-                 [30, 40, 50, 60, 70]])
-print('Image Matrix:')
+import numpy as np
+
+image = np.array([[10, 20, 30, 40, 50],
+                  [15, 25, 35, 45, 55],
+                  [20, 30, 40, 50, 60],
+                  [25, 35, 45, 55, 65],
+                  [30, 40, 50, 60, 70]])
+
+print("Image Matrix:\n")
 print(image)
 
-x,y = 2,2    # center pixels
-print('Selected Pixels: ')
-print(f'Position = {x}, { y}')
-print(f'value = {image[x, y]}')
+x, y = 2, 2
+
+print("\nSelected Pixel:")
+print(f"Position = ({x}, {y})")
+print(f"Value = {image[x, y]}")
+
 
 N4 = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-ND = [(x-1, y-1), (x-1, y+1), (x+1, y-1), (x+1, y+1)]
+
+ND = [(x-1, y-1), (x-1, y+1),
+      (x+1, y-1), (x+1, y+1)]
+
 N8 = N4 + ND
-Nm = [pixel for pixel in N4 if pixel not in ND]
 
-print(f'N4 (4-Neighbors): {N4}')
-print(f'N4 Values: {[image[r, c] for r, c in N4]}\n')
+print("\n4-Neighbors:")
+for i, j in N4:
+    print(f"({i}, {j}) = {image[i, j]}")
 
-print(f'ND (Diagonal Neighbors): {ND}')
-print(f'ND Values: {[image[r, c] for r, c in ND]}\n')
+print("\nDiagonal Neighbors:")
+for i, j in ND:
+    print(f"({i}, {j}) = {image[i, j]}")
 
-print(f'N8 (8-Neighbors): {N8}')
-print(f'N8 Values: {[image[r, c] for r, c in N8]}\n')
+print("\n8-Neighbors:")
+for i, j in N8:
+    print(f"({i}, {j}) = {image[i, j]}")
 
-print(f'Nm (Difference N4 - ND): {Nm}')
-print(f'Nm Values: {[image[r, c] for r, c in Nm]}\n')
+Nm = []
+for p in N4:
+    if p not in ND:
+        Nm.append(p)
+        
+print('\nM-neighbours (Difference N4 - ND)')
+for i, j in ND:
+    print(f"({i}, {j}) = {image[i, j]}")
